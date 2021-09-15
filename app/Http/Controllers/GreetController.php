d4e8540e5eb8fb35b0f01577108042cd12e232ad
Kazuo-Tsubokawa <hiwa1712@gmail.com>
2021-09-15 10:35:53 +0900

    インデント修正

--- app/Http/Controllers/GreetController.php
+++ app/Http/Controllers/GreetController.php
@@ -6,26 +6,28 @@ use Illuminate\Http\Request;
 
 class GreetController extends Controller
 {
-    public function greet($time) {
-    if ($time == 'morning') {
-        $title = '朝のあいさつ';
-        $greet = 'おはようございます';
-    } elseif ($time == 'afternoon') {
-        $title = '昼のあいさつ';
-        $greet = 'こんにちは';
-    } elseif ($time == 'evening') {
-        $title = '夕方のあいさつ';
-        $greet = 'こんばんは';
-    } elseif ($time == 'night') {
-        $title = '夜のあいさつ';
-        $greet = 'おやすみ';
-    } elseif ($time == 'random') {
-        $array = ['おはようございます', 'こんにちは', 'こんばんは', 'おやすみ'];
-        $title = 'ランダムなメッセージ';
-        $greet = $array[array_rand($array)];
-    }
-    return view('greet', [
-        'greet' => $greet,
-        'title' => $title]);
+    public function greet($time)
+    {
+        if ($time == 'morning') {
+            $title = '朝のあいさつ';
+            $greet = 'おはようございます';
+        } elseif ($time == 'afternoon') {
+            $title = '昼のあいさつ';
+            $greet = 'こんにちは';
+        } elseif ($time == 'evening') {
+            $title = '夕方のあいさつ';
+            $greet = 'こんばんは';
+        } elseif ($time == 'night') {
+            $title = '夜のあいさつ';
+            $greet = 'おやすみ';
+        } elseif ($time == 'random') {
+            $array = ['おはようございます', 'こんにちは', 'こんばんは', 'おやすみ'];
+            $title = 'ランダムなメッセージ';
+            $greet = $array[array_rand($array)];
+        }
+        return view('greet', [
+            'greet' => $greet,
+            'title' => $title
+        ]);
     }
 }
